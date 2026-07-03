5c6e60a3f950b9bbb5e2aa29581cdf9309fe9217
Gabriel <gabrielnumero47@gmail.com>
2026-07-03 18:12:16 -0300

    Removendo produtos

--- src/controllers/products-controller.ts
+++ src/controllers/products-controller.ts
@@ -1,9 +1,10 @@
 import { NextFunction, Request, Response } from "express";
+import { AppError } from "@/utils/AppError";
 import { knex } from "@/database/knex"
 import z from "zod";
 
 class ProductController {
-    async index(req: Request, res: Response, next: NextFunction){
+    async index(req: Request, res: Response, next: NextFunction) {
         try {
             const { name } = req.query
             const products = await knex<ProductRepository>("products")
@@ -16,7 +17,7 @@ class ProductController {
         }
     }
 
-    async create(req: Request, res: Response, next: NextFunction){
+    async create(req: Request, res: Response, next: NextFunction) {
         try {
             const bodySchema = z.object({
                 name: z.string().trim().min(6),
@@ -26,12 +27,67 @@ class ProductController {
             const { name, price } = bodySchema.parse(req.body)
 
             await knex<ProductRepository>("products").insert({ name, price })
-            
+
             return res.status(201).json()
+        } catch (error) {
+            next(error)
+        }
+    }
+
+    async update(req: Request, res: Response, next: NextFunction) {
+        try {
+            const id = z.string().transform((value) => Number(value))
+                .refine((value) => !isNaN(value), { message: "id must be a number" })
+                .parse(req.params.id)
+
+            const bodySchema = z.object({
+                name: z.string().trim().min(6),
+                price: z.number().gt(0)
+            })
+
+            const { name, price } = bodySchema.parse(req.body)
+
+            const product = await knex<ProductRepository>("products").select().where({ id }).first()
+
+            if(!product){
+                throw new AppError("product not found!")
+            }
+
+            await knex<ProductRepository>("products")
+                .update({ name, price, updated_at: knex.fn.now() })
+                .where({ id })
+
+            return res.json()
+        } catch (error) {
+            next(error)
+        }
+    }
+
+    async remove(req: Request, res: Response, next: NextFunction){
+        try {
+            const id = z
+                .string()
+                .transform((value) => Number(value))
+                .refine((value) => !isNaN(value), { message: "id must be a number" })
+                .parse(req.params.id)
+
+                const product = await knex<ProductRepository>("products")
+                    .select()
+                    .where({ id })
+                    .first()
+
+                    if(!product){
+                        throw new AppError("product not found")
+                    }
+
+                await knex<ProductRepository>("products").delete().where({ id })
+
+                return res.json()
+
         } catch (error) {
             next(error)
         }
     }
 }
 
-export {ProductController}
+export { ProductController }
