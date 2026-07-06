9348211ce4156accb403dcf3949896a91c766a23
Gabriel <gabrielnumero47@gmail.com>
2026-07-06 20:05:59 -0300

    Abrindo a mesa.

---
+++ src/routes/tables-sessions-routes.ts
@@ -0,0 +1,10 @@
+import { Router } from "express";
+
+import { TablesSessionsController } from "@/controllers/tables-sessions-controller";
+
+const tablesSessionsRoutes = Router()
+const tablesSessionController = new TablesSessionsController()
+
+tablesSessionsRoutes.post("/", tablesSessionController.create)
+
+export { tablesSessionsRoutes }
