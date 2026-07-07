be1b766a071d711063111847a5f7482d6d588e0d
Gabriel <gabrielnumero47@gmail.com>
2026-07-07 00:23:02 -0300

    Fechando a mesa.

--- src/routes/tables-sessions-routes.ts
+++ src/routes/tables-sessions-routes.ts
@@ -5,6 +5,8 @@ import { TablesSessionsController } from "@/controllers/tables-sessions-controll
 const tablesSessionsRoutes = Router()
 const tablesSessionController = new TablesSessionsController()
 
+tablesSessionsRoutes.get("/", tablesSessionController.index)
 tablesSessionsRoutes.post("/", tablesSessionController.create)
+tablesSessionsRoutes.patch("/:id", tablesSessionController.update)
 
 export { tablesSessionsRoutes }
